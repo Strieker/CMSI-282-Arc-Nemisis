975467ee4ce2500faef81c81d0c118c414aed19b
Sage Strieker <sspink157@gmail.com>
2019-05-08 22:36:48 -0700

    added an attempt at an arc consistency method

--- CSP.java
+++ CSP.java
@@ -12,12 +12,6 @@ import java.util.*;
  * some n meetings in a given period of time and according to some unary and
  * binary constraints on the dates of each meeting.
  */
-//
-//int nMeetings describing how many meetings need to be scheduled, and are indexed from 0 to n-1.
-//LocalDate rangeStart The start date (inclusive) of the domains of each of the n meeting-variables
-//LocalDate rangeEnd The end date (inclusive) of the domains of each of the n meeting-variables
-//Set<DateConstraint> constraints A Set of date constraints on the meeting times in the formats specified above.
-
 public class CSP {
 	
 	public static List<LocalDate> makeDateVars(int nMeetings){
@@ -47,8 +41,6 @@ public class CSP {
 		return domains;
 
 	}
-
-	// 2 DATES AND AN OPERATOR PASS : INDIVIDUAL CONSTRAINT CHECK
 	
 	private static boolean isConsistent(LocalDate leftDate, LocalDate rightDate, DateConstraint d) {
 		boolean sat = false;
@@ -104,6 +96,38 @@ public class CSP {
 		return domains;
 	}
 	
+	// FOR ANY ONE CONSTRAINT LOOK AT THE DOMAINS IT MENTIONS, LOOK AT ONE OF THE
+		// ARCS, AND EACH VALUE IN THE TAIL, SEE IF A VALUE IN THE HEAD SATISFIES THAT
+		// CONSTRAINT
+		// IF NO VALUES IN THE HEAD SATISFY THE CONSTRAINT FOR THAT TAIL VALUE, THEN
+		// PRUNE IT FROM THE TAIL DOMAIN
+
+		private static  List<List<LocalDate>> arcConsistency (List<List<LocalDate>> domains, Set<DateConstraint> constraints){
+			for (DateConstraint d : constraints) {
+				if (d.arity() == 1) {
+					continue;
+				}
+				for (int i = 0; i < domains.size(); i++) {
+					if (i == d.L_VAL) {
+						List<LocalDate> leftVal = domains.get(d.L_VAL);
+						List<LocalDate> rightVal = domains.get(((BinaryDateConstraint) d).R_VAL);
+						int indexMove = 0; 
+						boolean isConsistentSomewhere = false;
+						for(LocalDate domainL : leftVal) {
+							for(LocalDate domainR : rightVal) {
+								if(isConsistent(domainL, domainR, d)) {
+									isConsistentSomewhere = true;
+								} else if(!isConsistent(domainL, domainR, d) && indexMove == rightVal.size() - 1 && isConsistentSomewhere) {
+									rightVal.remove(domainL);
+								}
+							} 
+						}
+					}	
+				}
+			}
+			return domains;
+		}
+	
 	private static boolean constraintCheck(List<LocalDate> assignments, Set<DateConstraint> constraints) {
 		for (DateConstraint d : constraints) {
 			LocalDate leftDate = assignments.get(d.L_VAL);
@@ -118,59 +142,16 @@ public class CSP {
 				
 		return true;
 	}
-
-	
-
-	// FOR ANY ONE CONSTRAINT LOOK AT THE DOMAINS IT MENTIONS, LOOK AT ONE OF THE
-	// ARCS, AND EACH VALUE IN THE TAIL, SEE IF A VALUE IN THE HEAD SATISFIES THAT
-	// CONSTRAINT
-	// IF NO VALUES IN THE HEAD SATISFY THE CONSTRAINT FOR THAT TAIL VALUE, THEN
-	// PRUNE IT FROM THE TAIL DOMAIN
-
-//	private static List<List<LocalDate>> arcConstraint(List<List<LocalDate>> domains, Set<DateConstraint> constraints) {
-//
-//		for (DateConstraint d : constraints) {
-//
-//			if (d.arity() == 1) {
-//				continue;
-//			}
-//			LocalDate leftDate = null;
-//			LocalDate rightDate = null;
-//			LocalDate toRemove = null;
-//
-//			for (int i = 0; i < domains.size(); i++) {
-//				leftDate = domains.get(i).get(d.L_VAL);
-//				for (int j = 0; j < domains.size(); j++) {
-//					rightDate = domains.get(j).get(((BinaryDateConstraint) d).R_VAL);
-//					if (!constraintCheck(domains.get(i), constraints, true, toRemove, leftDate, rightDate)) {
-//						domains.get(i).remove(leftDate);
-//					}
-//				}
-//			}
-//
-//		}
-//		return domains;
-//	}
-//
 	
 	public static List<LocalDate> recursiveBackTracking(List<LocalDate> assignment, List<List<LocalDate>> domains,
 			Set<DateConstraint> constraints, int indexOfUnassignedVariable, int stopSize) {
-		// WHY ONLY 0?
-		System.out.println(indexOfUnassignedVariable);
 		if (indexOfUnassignedVariable == stopSize
 				&& constraintCheck(assignment, constraints)/* || domains.size() == 0 */) {
-			System.out.println("I GOT HERE 1");
-			for(LocalDate assig : assignment) {
-				System.out.println(assig);
-			}
 			return assignment;
 		}
-//        for(List<LocalDate> domain : domains) {
 		for (LocalDate value : domains.get(indexOfUnassignedVariable)) {
 			assignment.set(indexOfUnassignedVariable, value);
 			if (constraintCheck(assignment, constraints)) {
-				System.out.println("I GOT HERE 2");
-				System.out.println(assignment.toString());
 				List<LocalDate> result = recursiveBackTracking(assignment, domains, constraints,
 						indexOfUnassignedVariable + 1, stopSize);
 				if (result != null) {
@@ -204,16 +185,12 @@ public class CSP {
 			Set<DateConstraint> constraints) {
 //    	List<LocalDate> domains = makeDomains(nMeetings, rangeStart, rangeEnd);
 
-		List<List<LocalDate>> domains = nodeConsistency(makeDomains(nMeetings, rangeStart, rangeEnd), constraints);
-		for (List<LocalDate> date : domains) {
-			System.out.println("fuck");
-			System.out.println(date.toString());
-		}
+		List<List<LocalDate>> domains = arcConsistency(nodeConsistency(makeDomains(nMeetings, rangeStart, rangeEnd), constraints), constraints);
+		
 		List<LocalDate> assignment = makeDateVars(nMeetings);
     	List<LocalDate> solution = recursiveBackTracking(assignment, domains,constraints, 0, nMeetings);
 //
     	if(solution == null) {
-    		System.out.println("Solution was null");
     		return null;
     	} 
 		return solution;
